2f89b358438a19281278feda02c5b682bb9e5050
danielkutyin <daniel.kutyin@gmail.com>
2025-11-29 07:07:35 +0100

    Implémentation de la classe Position #2

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Position.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Position.java
@@ -1,4 +1,17 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 public class Position {
+    private double height;
+    private double width;
+
+    public Position(double height, double width){
+        this.height=height;
+        this.width=width;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public double getWidth(){
+        return width;
+    }
 }
