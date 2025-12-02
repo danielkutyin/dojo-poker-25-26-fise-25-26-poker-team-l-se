ae16d2e2f90bed50146a271752b2dcd69b7d4c84
Esser <esser.ghanmi@etu.unice.fr>
2025-12-02 13:25:36 +0100

    j'ai fait la classe Robot

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Robot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Robot.java
@@ -1,4 +1,15 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 public class Robot {
+    private final Engine engine;
+
+    public Robot(Engine engine){
+        this.engine = engine;
+    }
+
+    /** le robott donne l'ordre à l'engine pour placer la tuile aux coord choisies **/
+
+    public void placeTileAt (int height, int width, TileColor color){
+        engine.placeTile(height, width, color);
+    }
 }
