bf917c0819d2124069aaff55f1424ed97dc1cb10
Esser <esser.ghanmi@etu.unice.fr>
2025-12-02 13:13:50 +0100

    j'ai fait la classe Engine

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Engine.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Engine.java
@@ -1,4 +1,21 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 public class Engine {
+    private final Board board;
+
+    public Engine(Board board){
+        this.board = board;
+    }
+
+    /** on place la tuile (couleur donné) aux coordonnées (height, width) **/
+
+    /** cette fonction crée la position, la tuile puis elle appelle board.setTile **/
+    public void placeTile(int height, int width,TileColor color){
+        Position position = new Position(height, width);
+        Tile tile = new Tile(position, color);
+        board.setTile(tile,position);
+    }
+
+
+
 }
