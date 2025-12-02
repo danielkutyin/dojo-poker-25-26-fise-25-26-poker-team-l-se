42ce9d5b0a54e0b0f1c959a2f044b9f1388439ee
Esser <esser.ghanmi@etu.unice.fr>
2025-12-02 13:22:07 +0100

    j'ai testé la classe Engine et ça marche

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/EngineTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/EngineTest.java
@@ -1,4 +1,34 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class EngineTest {
+
+    @Test
+    void verificationplaceTile(){
+
+        Board.getHashMap().clear(); /** on part d'une map vide **/
+
+        Board board = new Board();
+        Engine engine = new Engine(board);
+        int height = 1;
+        int width = 2;
+        TileColor color = TileColor.GREEN;
+        engine.placeTile(height, width, color);
+
+        assertEquals(2, board.getNumTiles(), "Le plateau doit contenir 2 tuiles (l'étang + la tuile placée)");
+
+        Position position = new Position(height, width);
+        Tile tile = Board.getHashMap().get(position);
+
+        assertNotNull(tile, "Une tuile doit être présente à la position (1,2)");
+        assertEquals(TileColor.GREEN, tile.getColor(),  "La tuile placée doit être de couleur GREEN");
+
+
+
+    }
+
 }
