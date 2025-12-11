de26baf3fdb6556e6ea0c8eaa9416141089f53ee
Esser <esser.ghanmi@etu.unice.fr>
2025-12-11 21:27:53 +0100

    test: add tests for bamboo ( closes #15)

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/BambouTest.java
@@ -0,0 +1,23 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import fr.cotedazur.univ.polytech.startingpoint.takenoko.board.TileColor;
+import fr.cotedazur.univ.polytech.startingpoint.takenoko.elements.Bambou;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BambouTest {
+    @Test
+    public void testBambouKeepsColor() {
+        Bambou b = new Bambou(TileColor.GREEN);
+        assertEquals(TileColor.GREEN, b.getColor());
+    }
+
+    @Test
+    public void testToStringContainsColorName() {
+        Bambou b = new Bambou(TileColor.YELLOW);
+        String asString = b.toString();
+        assertTrue(asString.contains("YELLOW"));
+    }
+}
