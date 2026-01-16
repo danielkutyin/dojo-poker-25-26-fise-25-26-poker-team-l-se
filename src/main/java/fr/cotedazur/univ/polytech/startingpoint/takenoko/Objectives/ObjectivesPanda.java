4ae0ece60fd588b60716740c935147f884de432c
ZAIERWassef <wassef.zaier@etu.univ-cotedazur.fr>
2026-01-16 15:09:26 +0100

    fix: fix import

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/takenoko/Objectives/ObjectivesPanda.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/takenoko/Objectives/ObjectivesPanda.java
@@ -1,7 +1,8 @@
-package fr.cotedazur.univ.polytech.startingpoint.takenoko.Objectives;
+package fr.cotedazur.univ.polytech.startingpoint.takenoko.objectives;
 
 
 
+import fr.cotedazur.univ.polytech.startingpoint.takenoko.Objectives.PandaObjective;
 import fr.cotedazur.univ.polytech.startingpoint.takenoko.board.TileColor;
 
 import java.util.*;
